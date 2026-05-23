4649ca9ab4b6a178ae6a45a43044c12f828c673e
khangngo <nnd.khangvn@gmail.com>
2026-05-23 21:26:01 +0700

    Add LF TCs 009-012

---
+++ src/test/java/testCases/TC_LF_010_BackingAfterLoginTest.java
@@ -0,0 +1,50 @@
+package testCases;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import pageObjects.HomePage;
+import pageObjects.LoginPage;
+import pageObjects.MyAccountPage;
+import testBase.BaseClass;
+
+public class TC_LF_010_BackingAfterLoginTest extends BaseClass {
+
+	@Test(groups = {"login", "master"})
+	public void verify_backing_after_logout() {
+		logger.info("***Starting TC_LF_010_BackingAfterLoginTest ***");
+
+		try {
+			// HomePage actions
+			HomePage hp = new HomePage(driver);
+			hp.clickMyAccount();
+			hp.clickLogin();
+
+			// LoginPage actions
+			LoginPage lp = new LoginPage(driver);
+			lp.setEmail(p.getProperty("email1"));
+			lp.setPassword(p.getProperty("password"));
+			lp.clickLogin();
+
+			// MyAccountPage actions
+			MyAccountPage map = new MyAccountPage(driver);
+			map.clickLogout();
+			
+			driver.navigate().back(); //backing on browser
+			driver.navigate().refresh(); //refresh page to validate login status
+
+			boolean targetPage = map.isMyAccountHeadingExist();
+
+			// validation
+			Assert.assertEquals(targetPage, false, "Logout faieled");
+
+		} catch (Exception e) {
+			logger.debug(e.getMessage());
+			Assert.fail();
+		}
+
+		logger.info("***Finished TC_LF_010_BackingAfterLoginTest ***");
+
+	}
+
+}
